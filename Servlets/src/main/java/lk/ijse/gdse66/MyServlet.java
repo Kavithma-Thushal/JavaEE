f8f3888a5fdcb554d85fcdeb8533cedf3503f157
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-01-04 23:58:42 +0530

    Servlets Implementing

---
+++ Servlets/src/main/java/lk/ijse/gdse66/MyServlet.java
@@ -0,0 +1,19 @@
+package lk.ijse.gdse66;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : JavaEE
+ * @since : 11:53 PM - 1/4/2024
+ **/
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("MyServlet 1");
+    }
+}
