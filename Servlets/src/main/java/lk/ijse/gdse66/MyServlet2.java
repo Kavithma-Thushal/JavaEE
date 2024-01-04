aa78adf52f3439809ee7e015edcead0645adac4a
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-01-05 00:05:42 +0530

    Servlets Implemented

--- Servlets/src/main/java/lk/ijse/gdse66/MyServlet2.java
+++ Servlets/src/main/java/lk/ijse/gdse66/MyServlet2.java
@@ -1,5 +1,7 @@
 package lk.ijse.gdse66;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,5 +17,13 @@ public class MyServlet2 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("MyServlet 2");
+
+        ServletConfig servletConfig = getServletConfig();
+        String name = servletConfig.getInitParameter("name");
+        System.out.println(name);
+
+        ServletContext servletContext = getServletContext();
+        String city = servletContext.getInitParameter("city");
+        System.out.println(city);
     }
 }
