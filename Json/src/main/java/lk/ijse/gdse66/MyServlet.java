9918472b49d1b9b82fece61a92b571ca7e06d176
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-01-05 19:30:27 +0530

    Json Implemented

--- Json/src/main/java/lk/ijse/gdse66/MyServlet.java
+++ Json/src/main/java/lk/ijse/gdse66/MyServlet.java
@@ -1,10 +1,16 @@
 package lk.ijse.gdse66;
 
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonObjectBuilder;
+import jakarta.json.JsonReader;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 
 /**
@@ -16,6 +22,27 @@ import java.io.IOException;
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Json");
+
+        /*-------------------- JSON Read --------------------*/
+
+        /*Without Json Library*/
+        BufferedReader reader = req.getReader();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        /*Using JsonP Library*/
+        JsonReader reader1 = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader1.readObject();
+        System.out.println(jsonObject);
+
+        /*-------------------- JSON Write --------------------*/
+        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+        objectBuilder.add("id", "C001");
+        objectBuilder.add("name", "Kamal");
+        objectBuilder.add("address", "Panadura");
+        JsonObject jsonObject2 = objectBuilder.build();
+        resp.getWriter().write(jsonObject2.toString());
     }
 }
