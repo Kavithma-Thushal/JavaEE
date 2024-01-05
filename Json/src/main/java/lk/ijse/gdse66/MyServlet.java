08a3c3bcdd7be9108a6e096e50aabff99b306db6
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-01-05 19:16:26 +0530

    Json Implementing

---
+++ Json/src/main/java/lk/ijse/gdse66/MyServlet.java
@@ -0,0 +1,21 @@
+package lk.ijse.gdse66;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : JavaEE
+ * @since : 7:11 PM - 1/5/2024
+ **/
+@WebServlet(urlPatterns = "/test")
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Json");
+    }
+}
