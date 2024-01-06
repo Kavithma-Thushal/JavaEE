48b57b580aaa7a501222e9c4060df7b283a2826c
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-01-06 07:40:52 +0530

    POS Implementing

---
+++ POS/src/main/java/lk/ijse/gdse66/pos/customers.java
@@ -0,0 +1,21 @@
+package lk.ijse.gdse66.pos;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : JavaEE
+ * @since : 7:38 AM - 1/6/2024
+ **/
+@WebServlet("/customers")
+public class customers extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("POS");
+    }
+}
