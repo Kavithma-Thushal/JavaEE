89e3eb87c55e0088a28f68e160440e1cb252de64
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-01-06 16:52:24 +0530

    Json Added

--- POS/src/main/java/lk/ijse/gdse66/pos/customers.java
+++ POS/src/main/java/lk/ijse/gdse66/pos/customers.java
@@ -1,15 +1,14 @@
 package lk.ijse.gdse66.pos;
 
+import jakarta.json.*;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * @author : Kavithma Thushal
@@ -18,17 +17,24 @@ import java.sql.SQLException;
  **/
 @WebServlet("/customers")
 public class customers extends HttpServlet {
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String id = req.getParameter("id");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*String id = req.getParameter("id");
         String name = req.getParameter("name");
-        String address = req.getParameter("address");
+        String address = req.getParameter("address");*/
 
-        System.out.println("doGet()");
-        System.out.println(id + " - " + address + " - " + name);
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+        String id = jsonObject.getString("id");
+        String name = jsonObject.getString("name");
+        String address = jsonObject.getString("address");
 
-        resp.getWriter().println("doGet()");
-        resp.getWriter().println(id + " - " + address + " - " + name);
+        System.out.println("doPost()");
+        System.out.println(id + " - " + name + " - " + address);
+
+        resp.getWriter().println("doPost()");
+        resp.getWriter().println(id + " - " + name + " - " + address);
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -47,27 +53,48 @@ public class customers extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String id = req.getParameter("id");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*String id = req.getParameter("id");
         String name = req.getParameter("name");
         String address = req.getParameter("address");
 
-        System.out.println("doPost()");
-        System.out.println(id + " - " + name + " - " + address);
+        System.out.println("doGet()");
+        System.out.println(id + " - " + address + " - " + name);
 
-        resp.getWriter().println("doPost()");
-        resp.getWriter().println(id + " - " + name + " - " + address);
+        resp.getWriter().println("doGet()");
+        resp.getWriter().println(id + " - " + address + " - " + name);*/
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/servlet_db", "root", "1234");
 
-            String sql = "INSERT INTO customer(id, name, address) VALUES (?,?,?)";
+            /*String sql = "INSERT INTO customer(id, name, address) VALUES (?,?,?)";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setString(1, id);
             preparedStatement.setString(2, name);
             preparedStatement.setString(3, address);
-            preparedStatement.executeUpdate();
+            preparedStatement.executeUpdate();*/
+
+            JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+            String sql = "SELECT * FROM customer";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                String name = resultSet.getString("name");
+                String address = resultSet.getString("address");
+
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                objectBuilder.add("id", id);
+                objectBuilder.add("name", name);
+                objectBuilder.add("address", address);
+                JsonObject customerObject = objectBuilder.build();
+                arrayBuilder.add(customerObject);
+            }
+
+            JsonArray customerArray = arrayBuilder.build();
+            resp.getWriter().println(customerArray.toString());
+            resp.setContentType("application/json");
 
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
